ba2bee3105a437e7446d63b890d85671c9d67b34
PrerakPatelCS <patelprerak121@gmail.com>
2021-01-31 13:47:27 -0500

    testing commit

--- ASST1/ASST1/src/Kiosk.java
+++ ASST1/ASST1/src/Kiosk.java
@@ -1,4 +1,18 @@
+/*
+TODO / NOTES 
+     ** delete this section before submission**
 
+     - use RunProject1.java as a driver
+
+     - kiosk processes command lines from console
+        - user interface class handling input/output
+    - you can define the data members you need and define some private methods to handle the commands
+        - follow ground rules and programming style.
+
+
+*/
 public class Kiosk {
+
 	public void run() { }
+
 }
