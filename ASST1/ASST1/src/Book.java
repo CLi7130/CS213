0bf00ab3e08c690a0ff50e77abaec2cc4b3d3cd2
PrerakPatelCS <patelprerak121@gmail.com>
2021-01-31 00:24:33 -0500

    Book with getters and setters

--- ASST1/ASST1/src/Book.java
+++ ASST1/ASST1/src/Book.java
@@ -1,5 +1,49 @@
+/* 
 
+TODO / Notes on Book class:
+
+    ** delete this section before submission**
+
+    - implement equals method
+        - method checks book number
+    - implement toString method
+        - format: "Book#10007::Design Patterns::5/30/1996::is available."
+                : "Book#<Book.number>::<Book.name>::<Book.datePublished>
+                    ::<is/is not available>."
+                    - for isAvailable:  - check Book.checkedOut
+                                        - check if book is in library at all
+
+    - do not add any other data members/instance variables EXCEPT static variables 
+        or constants
+    - You CANNOT read from console, or use System.out in this class
+        - always return to kiosk class-> read from console/print there?
+
+    - Book.number   
+        - starts at 10001
+        - will not exceed 5 digits per Project 1 Q&A
+        - implement counter in Kiosk class to track # of books?
+            - start counter at 10000
+            - if command is A / Add - increment bookCounter
+                - Book.number = bookCounter;
+
+    Book.name
+        - check for empty string
+
+    Book.checkedOut
+        - initialize to false
+        - make sure to change when checking books in/out
+        
+*/
+
+/** 
+    This class instantiates a book object to be placed in the library bag data structure,and creates two methods useful in categorization and parsing of the given library.
+    The book class contains instance variables that encompass the identifiers of a unique book available in the library, including a unique five digit serial number (Book.number), the book's title (Book.name), whether the book is checked in or out of the library (Book.checkedOut), and the publish date 
+    (Book.datePublished). Other methods included are the equals(Object obj) method and the toString() method, which check if a given book is equivalent to another, or convert relevant information about the book into a String of format "Book#<Book.number>::<Book.name>::<Book.datePublished>::<is/is not available>.".
+
+    @author Craig Li, Prerak Patel
+ */
 public class Book {
+
 	private String number; //a 5-digit serial number unique to the book
 	private String name;
 	private boolean checkedOut;
@@ -32,9 +76,58 @@ public class Book {
 	public void setDatePublished(Date datePublished) {
 		this.datePublished = datePublished;
 	}
-	
-	@Override
-	public boolean equals(Object obj){ }
-	@Override
-	public String toString() { }
+    
+    /**
+     Checks if two book objects are equivalent by comparing Book name and date published.
+     @param book to compare against
+     @return true if books compared have same name and publish date, false 
+             if otherwise
+     */
+    @Override
+    public boolean equals(Object obj){
+        //compare name, date published, maybe book number?
+        return false;
+    }
+    /**
+     * Returns formatted string containing book's number, name, date published,
+     * and availability.
+     * @return String formatted to a specific book, following the format of: 
+     *         "Book#<Book.number>::<Book.name>::<Book.datePublished>::<is/is 
+     *         not available>.".
+     */
+    @Override
+    public String toString(){
+        String formattedBookInfo = new String("Book#");
+        formattedBookInfo = formattedBookInfo + this.number;
+        formattedBookInfo = formattedBookInfo + "::";
+        formattedBookInfo = formattedBookInfo + this.name;
+        formattedBookInfo = formattedBookInfo + "::";
+        formattedBookInfo = formattedBookInfo + this.datePublished;
+        //this.datePublished - need to reformat so it prints date object
+        formattedBookInfo = formattedBookInfo + "::";
+        if(!checkedOut){
+            formattedBookInfo = formattedBookInfo + "is available.";
+        }
+        else{
+            formattedBookInfo = formattedBookInfo + "is not available.";
+        }
+
+        return formattedBookInfo;
+    }
+
+
+//test driver, delete when making final formatting pass
+public static void main(String[] args){
+    Book testBook = new Book();
+    testBook.name = "Born a Crime";
+    testBook.datePublished = new Date("11/15/2016");
+    System.out.println(String.valueOf(testBook.datePublished));
+    //need to write date class for this to work - current implementation doesn't
+        //print date correctly
+    testBook.number = "10001";
+    testBook.checkedOut = false;
+
+    //toString method tested, all fields but datePublished work
+    System.out.println(testBook.toString());
+}
 }
