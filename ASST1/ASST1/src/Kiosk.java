d788cc7f1152209a51e393841715ad7de675e689
PrerakPatelCS <patelprerak121@gmail.com>
2021-02-01 15:21:28 -0500

    Started to read inputs

--- ASST1/ASST1/src/Kiosk.java
+++ ASST1/ASST1/src/Kiosk.java
@@ -1,18 +1,31 @@
+import java.util.Scanner;
+import java.util.StringTokenizer;
+
 /*
 TODO / NOTES 
      ** delete this section before submission**
-
      - use RunProject1.java as a driver
-
      - kiosk processes command lines from console
         - user interface class handling input/output
     - you can define the data members you need and define some private methods to handle the commands
         - follow ground rules and programming style.
-
-
 */
 public class Kiosk {
 
-	public void run() { }
+    public void run(){
+    	System.out.print("Library Kiosk running ");
+        String firstToken = "";
+
+        while(!firstToken.contentEquals("Q")) {
+            Scanner input = new Scanner(System.in);
+            String stringInput = input.nextLine();
+            StringTokenizer string = new StringTokenizer(stringInput);
+            firstToken = string.nextToken();
+            System.out.println(firstToken);
+
+        }
+    	
+    	
+    }
 
 }
