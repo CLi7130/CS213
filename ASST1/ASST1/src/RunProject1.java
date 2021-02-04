7d452570112461216ff2412d53754a679cf9507c
PrerakPatelCS <patelprerak121@gmail.com>
2021-02-03 20:22:31 -0500

    Updated header

--- ASST1/ASST1/src/RunProject1.java
+++ ASST1/ASST1/src/RunProject1.java
@@ -1,9 +1,8 @@
-  
-/*
-TODO / NOTES 
-     ** delete this section before submission**
-     - implement header
+/**
+	This class the is driver class that runs the whole project.
+@author Craig Li, Prerak Patel
 */
+
 public class RunProject1 {
 
 	public static void main(String[] args) {
