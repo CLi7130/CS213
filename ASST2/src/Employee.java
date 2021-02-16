3608c2745fda6e23c93dddf8ba911303e6df2ac2
Craig Li <Cli7130@gmail.com>
2021-02-16 12:11:20 -0500

    Added printByDate/printByDept

--- ASST2/src/Employee.java
+++ ASST2/src/Employee.java
@@ -13,7 +13,6 @@ import java.text.DecimalFormat;
 
 public class Employee{
     Profile profile = new Profile();
-    DecimalFormat money = new DecimalFormat("0.00");
     
     public Employee(Profile Profile) {
         profile = Profile;
