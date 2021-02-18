726325fed94dfa64f57c8ea7f6e0033265d6a597
PrerakPatelCS <patelprerak121@gmail.com>
2021-02-17 22:10:05 -0500

    CompanyTest

---
+++ ASST2/src/JUnitTests/CompanyTest.java
@@ -0,0 +1,40 @@
+/**
+ * 
+ */
+package JUnitTests;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+/**
+ * @author Prerak
+ *
+ */
+public class CompanyTest {
+
+    /**
+     * Test method for {@link Company#add(Employee)}.
+     */
+    @Test
+    public void testAdd() {
+        //fail("Not yet implemented");
+    }
+
+    /**
+     * Test method for {@link Company#remove(Employee)}.
+     */
+    @Test
+    public void testRemove() {
+        //fail("Not yet implemented");
+    }
+
+    /**
+     * Test method for {@link Company#setHours(Employee)}.
+     */
+    @Test
+    public void testSetHours() {
+        //fail("Not yet implemented");
+    }
+
+}
