b8e5aa058b0490e19fe0d8453c5623852d2c2ce3
PrerakPatelCS <patelprerak121@gmail.com>
2021-02-17 22:19:08 -0500

    ManagementTest

---
+++ ASST2/src/JUnitTests/ManagementTest.java
@@ -0,0 +1,24 @@
+/**
+ * 
+ */
+package JUnitTests;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+/**
+ * @author Prerak
+ *
+ */
+public class ManagementTest {
+
+    /**
+     * Test method for {@link Management#calculatePayment()}.
+     */
+    @Test
+    public void testCalculatePayment() {
+        //fail("Not yet implemented");
+    }
+
+}
