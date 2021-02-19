a5928e28e54b40d6f2db5b1ae8ed7f99df86c76b
PrerakPatelCS <patelprerak121@gmail.com>
2021-02-19 01:33:34 -0500

    Testing

--- ASST2/src/ManagementTest.java
+++ ASST2/src/ManagementTest.java
@@ -42,13 +42,41 @@ public class ManagementTest {
         profile3.setDateHired(date3);
         employee3.setRole(3);
         employee3.setAnnualSalary(85000);
+        
+        Profile profile4 = new Profile();
+        Management employee4 = new Management(profile4);
+        Date date4 = new Date("7/1/2018");
+        profile4.setName("Kyle");
+        profile4.setDepartment("CS");
+        profile4.setDateHired(date4);
+        employee4.setRole(1);
+        Profile profile5 = new Profile();
+        Management employee5 = new Management(profile5);
+        Date date5 = new Date("9/23/1989");
+        profile5.setName("David");
+        profile5.setDepartment("IT");
+        profile5.setDateHired(date5);
+        employee5.setRole(2);
+        Profile profile6 = new Profile();
+        Management employee6 = new Management(profile6);
+        Date date6 = new Date("10/5/2015");
+        profile6.setName("Peter");
+        profile6.setDepartment("ECE");
+        profile6.setDateHired(date6);
+        employee6.setRole(3);
+        
         employee1.calculatePayment();
         employee2.calculatePayment();
         employee3.calculatePayment();
+        employee4.calculatePayment();
+        employee5.calculatePayment();
+        employee6.calculatePayment();
         assertEquals(employee1.getPayment(), 3461.54, 0.01); //Test Case #1 payment for Manager
         assertEquals(employee2.getPayment(), 3634.61, 0.01); //Test Case #2 payment for Department Head
         assertEquals(employee3.getPayment(), 3730.77, 0.01); //Test Case #3 payment for Director
-        
+        assertEquals(employee4.getPayment(), 192.31, 0.01); //Test Case #4 payment for Manager with no annualSalary
+        assertEquals(employee5.getPayment(), 365.38, 0.01); //Test Case #5 payment for Department Head with no annualSalary
+        assertEquals(employee6.getPayment(), 461.54, 0.01); //Test Case #6 payment for Director with no annualSalary
         
         
     }
