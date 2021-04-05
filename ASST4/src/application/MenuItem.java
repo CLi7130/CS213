b50355fea57829a642aa0674028cb1776efef35b
PrerakPatelCS <62869089+PrerakPatelCS@users.noreply.github.com>
2021-04-05 02:56:19 -0400

    Update MenuItem.java

--- ASST4/src/application/MenuItem.java
+++ ASST4/src/application/MenuItem.java
@@ -1,20 +1,51 @@
 package application;
 
-public class MenuItem implements Customizable{
+import java.text.DecimalFormat;
+
+public class MenuItem implements Customizable {
+
+    private int quantity;
+    final static double TAX = .06625;
+    MenuItem menuItem;
+    /**
+     * @return the quantity
+     */
+    public int getQuantity() {
+        return quantity;
+    }
+
+    /**
+     * @param quantity the quantity to set
+     */
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
 
     @Override
     public boolean add(Object obj) {
-        // TODO Auto-generated method stub
+        if (obj instanceof MenuItem) {
+            menuItem = (MenuItem) obj;
+            return true;
+        }
         return false;
     }
 
     @Override
     public boolean remove(Object obj) {
-        // TODO Auto-generated method stub
+        if (obj instanceof MenuItem) {
+            menuItem = (MenuItem) obj;
+            return true;
+        }
         return false;
     }
+
+    public double itemPrice() {
+        //return this.itemPrice() * quantity * TAX;
+        return 0;
+    }
     
-    public void itemPrice(){
-        
+    public String print() {
+        DecimalFormat money = new DecimalFormat("#,###.##");
+        return money.format(itemPrice());
     }
 }
