71f53cb14beb87537a02610be73e7aa70a58e798
PrerakPatelCS <62869089+PrerakPatelCS@users.noreply.github.com>
2021-04-05 20:53:13 -0400

    switched grande and venti

--- ASST4/src/application/Enums.java
+++ ASST4/src/application/Enums.java
@@ -6,22 +6,22 @@ package application;
 
 public class Enums{
 
-	protected enum COFFEE_SIZE {
-		SHORT, TALL, VENTI, GRANDE;
-	}
-	protected enum COFFEE_ADD_INS {
-		CREAM, SYRUP, MILK, CARAMEL, WHIPPED_CREAM;
-	}
-	protected enum DONUT_TYPES {
-		CAKE_DONUTS, DONUT_HOLES, YEAST_DONUTS;
-	}
-	protected enum CAKE_DONUTS {
-		BLUEBERRY, BOSTON_CREME, APPLE_CRUMB;
-	}
-	protected enum DONUT_HOLES {
-		CHOCOLATE_GLAZED, RASPBERRY_JELLY, POWDERED;
-	}
-	protected enum YEAST_DONUTS {
-		CHOCOLATE_FROSTED, GLAZED_CRULLER, MARBLE_FROSTED;
-	}
+    protected enum COFFEE_SIZE {
+        SHORT, TALL, GRANDE, VENTI;
+    }
+    protected enum COFFEE_ADD_INS {
+        CREAM, SYRUP, MILK, CARAMEL, WHIPPED_CREAM;
+    }
+    protected enum DONUT_TYPES {
+        CAKE_DONUTS, DONUT_HOLES, YEAST_DONUTS;
+    }
+    protected enum CAKE_DONUTS {
+        BLUEBERRY, BOSTON_CREME, APPLE_CRUMB;
+    }
+    protected enum DONUT_HOLES {
+        CHOCOLATE_GLAZED, RASPBERRY_JELLY, POWDERED;
+    }
+    protected enum YEAST_DONUTS {
+        CHOCOLATE_FROSTED, GLAZED_CRULLER, MARBLE_FROSTED;
+    }
 }
