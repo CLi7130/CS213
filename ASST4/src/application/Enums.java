92ceb26e6fb0020095d235e8a24db46ec09c41a6
PrerakPatelCS <62869089+PrerakPatelCS@users.noreply.github.com>
2021-04-06 23:00:53 -0400

    Comments Loaded

--- ASST4/src/application/Enums.java
+++ ASST4/src/application/Enums.java
@@ -5,23 +5,52 @@ package application;
  */
 
 public class Enums{
-
-	protected enum COFFEE_SIZE {
-		SHORT, TALL, GRANDE, VENTI;
-	}
-	protected enum COFFEE_ADD_INS {
-		CREAM, SYRUP, MILK, CARAMEL, WHIPPED_CREAM;
-	}
-	protected enum DONUT_TYPES {
-		CAKE_DONUTS, DONUT_HOLES, YEAST_DONUTS;
-	}
-	protected enum CAKE_DONUTS {
-		BLUEBERRY, BOSTON_CREME, APPLE_CRUMB;
-	}
-	protected enum DONUT_HOLES {
-		CHOCOLATE_GLAZED, RASPBERRY_JELLY, POWDERED;
-	}
-	protected enum YEAST_DONUTS {
-		CHOCOLATE_FROSTED, GLAZED_CRULLER, MARBLE_FROSTED;
-	}
+    
+    /**
+     * Provides enum constants for coffee sizes.
+     * @author Craig Li, Prerak Patel
+     */
+    protected enum COFFEE_SIZE {
+        SHORT, TALL, GRANDE, VENTI;
+    }
+    
+    /**
+     * Provides enum constants for coffee add ins.
+     * @author Craig Li, Prerak Patel
+     */
+    protected enum COFFEE_ADD_INS {
+        CREAM, SYRUP, MILK, CARAMEL, WHIPPED_CREAM;
+    }
+    
+    /**
+     * Provides enum constants for donut types.
+     * @author Craig Li, Prerak Patel
+     */
+    protected enum DONUT_TYPES {
+        CAKE_DONUTS, DONUT_HOLES, YEAST_DONUTS;
+    }
+    
+    /**
+     * Provides enum constants for cake donut flavors.
+     * @author Craig Li, Prerak Patel
+     */
+    protected enum CAKE_DONUTS {
+        BLUEBERRY, BOSTON_CREME, APPLE_CRUMB;
+    }
+    
+    /**
+     * Provides enum constants for donut hole flavors.
+     * @author Craig Li, Prerak Patel
+     */
+    protected enum DONUT_HOLES {
+        CHOCOLATE_GLAZED, RASPBERRY_JELLY, POWDERED;
+    }
+    
+    /**
+     * Provides enum constants for yeast donut flavors.
+     * @author Craig Li, Prerak Patel
+     */
+    protected enum YEAST_DONUTS {
+        CHOCOLATE_FROSTED, GLAZED_CRULLER, MARBLE_FROSTED;
+    }
 }
